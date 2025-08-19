15b838dd8ace0d3f43d520bb264befb6004447ef
AlexSi <a.siryulin@gmail.com>
2025-08-20 01:33:09 +0300

    #5 commit

--- ScanPerson/Scanperson.DAL/Contexts/ScanPersonDbContext.cs
+++ ScanPerson/Scanperson.DAL/Contexts/ScanPersonDbContext.cs
@@ -1,10 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
-using ScanPerson.DAL2.Migrations._2024_12;
+using ScanPerson.DAL.Migrations._2024_12;
 
 using ScanPerson.Models.Entities;
 
-namespace ScanPerson.DAL2.Contexts
+namespace ScanPerson.DAL.Contexts
 {
 	public class ScanPersonDbContext : DbContext
 	{
