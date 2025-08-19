f79d4079748556cc25303f85467bdc5c5a43cc8c
AlexSi <a.siryulin@gmail.com>
2025-08-20 01:13:49 +0300

    #5 mock failed dependicies for testing

--- ScanPerson/ScanPerson.BusinessLogic/Services/PersonService.cs
+++ ScanPerson/ScanPerson.BusinessLogic/Services/PersonService.cs
@@ -2,7 +2,6 @@
 
 using Microsoft.Extensions.Logging;
 
-using ScanPerson.DAL.Contexts;
 
 using ScanPerson.Common.Resources;
 using ScanPerson.Models.Items;
@@ -12,13 +11,14 @@ using ScanPerson.Models.Responses;
 namespace ScanPerson.BusinessLogic.Services
 {
 	public class PersonService(
-		ILogger<PersonService> logger,
-		ScanPersonDbContext context) : IPersonService
+		ILogger<PersonService> logger
+		) : IPersonService
 	{
 		public ScanPersonResultResponse<PersonItem[]?> Query(PersonRequest request)
 		{
 			logger.LogInformation(string.Format(Messages.StartedMethod, MethodBase.GetCurrentMethod()));
-			var result = new ScanPersonResultResponse<PersonItem[]?>(context.Persons?.Select(x => new PersonItem(x.Id, x.Name, x.Mail))?.ToArray());
+			//var result = new ScanPersonResultResponse<PersonItem[]?>(context.Persons?.Select(x => new PersonItem(x.Id, x.Name, x.Mail))?.ToArray());
+			var result = new ScanPersonResultResponse<PersonItem[]?>([new PersonItem(1, "x.Name", "x.Mail")]);
 
 			return result;
 		}
@@ -26,9 +26,10 @@ namespace ScanPerson.BusinessLogic.Services
 		public ScanPersonResultResponse<PersonItem?>? Find(PersonRequest request)
 		{
 			logger.LogInformation(string.Format(Messages.StartedMethod, MethodBase.GetCurrentMethod()));
-			var result = context.Persons?
-				.Select(x => new ScanPersonResultResponse<PersonItem?>(new PersonItem(x.Id, x.Name, x.Mail)))
-				.FirstOrDefault();
+			//var result = context.Persons?
+			//	.Select(x => new ScanPersonResultResponse<PersonItem?>(new PersonItem(x.Id, x.Name, x.Mail)))
+			//	.FirstOrDefault();
+			var result = new ScanPersonResultResponse<PersonItem?>(new PersonItem(1, "x.Name", "x.Mail"));
 
 			return result;
 		}
