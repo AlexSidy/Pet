400193f88ab675bb52d04dcb168853d4f6389f39
AlexSi <a.siryulin@gmail.com>
2025-10-14 01:04:32 +0300

    #83 fix: fix sonar warnings

--- ScanPerson/ScanPerson.Common/MapperProfiles/GrpcProfile.cs
+++ ScanPerson/ScanPerson.Common/MapperProfiles/GrpcProfile.cs
@@ -19,24 +19,44 @@ namespace ScanPerson.Common.MapperProfiles
 		}
 
 		/// <summary>
+		/// Check and get null if empty string.
+		/// </summary>
+		/// <param name="value">Source field value.</param>
+		/// <returns>Value or null if source is null or empty.</returns>
+		private static string? GetNullIfNullOrEmpty(string? value)
+		{
+			return string.IsNullOrEmpty(value) ? null : value;
+		}
+
+		/// <summary>
+		/// Check and get empty if value is null.
+		/// </summary>
+		/// <param name="value">Source field value.</param>
+		/// <returns>Value or empty if source is null.</returns>
+		private static string? GetEmptyIfNull(string? value)
+		{
+			return value ?? string.Empty;
+		}
+
+		/// <summary>
 		/// Configurate mapping project entities to gprc enttities.
 		/// </summary>
 		private void SetupProjectModelsToGprcMapping()
 		{
 			CreateMap<GrpcPersonInfoRequest, PersonInfoRequest>();
 			CreateMap<GrpcScanPersonResponse, ScanPersonResponseBase>()
-				.ForMember(dest => dest.Error, opt => opt.MapFrom(src => string.IsNullOrEmpty(src.Error) ? null : src.Error));
+				.ForMember(dest => dest.Error, opt => opt.MapFrom(src => GetNullIfNullOrEmpty(src.Error)));
 			CreateMap<GrpcScanPersonResponse, ScanPersonResultResponse<PersonInfoItem>>()
-				.ForMember(dest => dest.Error, opt => opt.MapFrom(src => string.IsNullOrEmpty(src.Error) ? null : src.Error));
+				.ForMember(dest => dest.Error, opt => opt.MapFrom(src => GetNullIfNullOrEmpty(src.Error)));
 			CreateMap<GrpcPersonInfoItem, PersonInfoItem>()
-				.ForMember(dest => dest.Mail, opt => opt.MapFrom(src => string.IsNullOrEmpty(src.Mail) ? null : src.Mail));
+				.ForMember(dest => dest.Mail, opt => opt.MapFrom(src => GetNullIfNullOrEmpty(src.Mail)));
 			CreateMap<GrpcLocationItem, LocationItem>()
-				.ForMember(dest => dest.CountryName, opt => opt.MapFrom(src => string.IsNullOrEmpty(src.CountryName) ? null : src.CountryName))
-				.ForMember(dest => dest.CurrentRegion, opt => opt.MapFrom(src => string.IsNullOrEmpty(src.CurrentRegion) ? null : src.CurrentRegion))
-				.ForMember(dest => dest.RegistrationOkrug, opt => opt.MapFrom(src => string.IsNullOrEmpty(src.RegistrationOkrug) ? null : src.RegistrationOkrug))
-				.ForMember(dest => dest.RegistrationCapital, opt => opt.MapFrom(src => string.IsNullOrEmpty(src.RegistrationCapital) ? null : src.RegistrationCapital))
-				.ForMember(dest => dest.OperatorCity, opt => opt.MapFrom(src => string.IsNullOrEmpty(src.OperatorCity) ? null : src.OperatorCity))
-				.ForMember(dest => dest.OperatorName, opt => opt.MapFrom(src => string.IsNullOrEmpty(src.OperatorName) ? null : src.OperatorName));
+				.ForMember(dest => dest.CountryName, opt => opt.MapFrom(src => GetNullIfNullOrEmpty(src.CountryName)))
+				.ForMember(dest => dest.CurrentRegion, opt => opt.MapFrom(src => GetNullIfNullOrEmpty(src.CurrentRegion)))
+				.ForMember(dest => dest.RegistrationOkrug, opt => opt.MapFrom(src => GetNullIfNullOrEmpty(src.RegistrationOkrug)))
+				.ForMember(dest => dest.RegistrationCapital, opt => opt.MapFrom(src => GetNullIfNullOrEmpty(src.RegistrationCapital)))
+				.ForMember(dest => dest.OperatorCity, opt => opt.MapFrom(src => GetNullIfNullOrEmpty(src.OperatorCity)))
+				.ForMember(dest => dest.OperatorName, opt => opt.MapFrom(src => GetNullIfNullOrEmpty(src.OperatorName)));
 		}
 
 		/// <summary>
@@ -45,19 +65,19 @@ namespace ScanPerson.Common.MapperProfiles
 		private void SetupGprcToProjectModelsMapping()
 		{
 			CreateMap<ScanPersonResponseBase, GrpcScanPersonResponse>()
-							.ForMember(dest => dest.Error, opt => opt.MapFrom(src => src.Error ?? string.Empty))
-							.ForMember(dest => dest.Result, opt => opt.Ignore());
+				.ForMember(dest => dest.Error, opt => opt.MapFrom(src => GetEmptyIfNull(src.Error)))
+				.ForMember(dest => dest.Result, opt => opt.Ignore());
 			CreateMap<ScanPersonResultResponse<PersonInfoItem>, GrpcScanPersonResponse>()
-				.ForMember(dest => dest.Error, opt => opt.MapFrom(src => src.Error ?? string.Empty));
+				.ForMember(dest => dest.Error, opt => opt.MapFrom(src => GetEmptyIfNull(src.Error)));
 			CreateMap<PersonInfoItem, GrpcPersonInfoItem>()
-				.ForMember(dest => dest.Mail, opt => opt.MapFrom(src => src.Mail ?? string.Empty));
+				.ForMember(dest => dest.Mail, opt => opt.MapFrom(src => GetEmptyIfNull(src.Mail)));
 			CreateMap<LocationItem, GrpcLocationItem>()
-				.ForMember(dest => dest.CountryName, opt => opt.MapFrom(src => src.CountryName ?? string.Empty))
-				.ForMember(dest => dest.CurrentRegion, opt => opt.MapFrom(src => src.CurrentRegion ?? string.Empty))
-				.ForMember(dest => dest.RegistrationOkrug, opt => opt.MapFrom(src => src.RegistrationOkrug ?? string.Empty))
-				.ForMember(dest => dest.RegistrationCapital, opt => opt.MapFrom(src => src.RegistrationCapital ?? string.Empty))
-				.ForMember(dest => dest.OperatorCity, opt => opt.MapFrom(src => src.OperatorCity ?? string.Empty))
-				.ForMember(dest => dest.OperatorName, opt => opt.MapFrom(src => src.OperatorName ?? string.Empty));
+				.ForMember(dest => dest.CountryName, opt => opt.MapFrom(src => GetEmptyIfNull(src.CountryName)))
+				.ForMember(dest => dest.CurrentRegion, opt => opt.MapFrom(src => GetEmptyIfNull(src.CurrentRegion)))
+				.ForMember(dest => dest.RegistrationOkrug, opt => opt.MapFrom(src => GetEmptyIfNull(src.RegistrationOkrug)))
+				.ForMember(dest => dest.RegistrationCapital, opt => opt.MapFrom(src => GetEmptyIfNull(src.RegistrationCapital)))
+				.ForMember(dest => dest.OperatorCity, opt => opt.MapFrom(src => GetEmptyIfNull(src.OperatorCity)))
+				.ForMember(dest => dest.OperatorName, opt => opt.MapFrom(src => GetEmptyIfNull(src.OperatorName)));
 		}
 	}
 }
